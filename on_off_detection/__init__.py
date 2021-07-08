b8ce4e39e38331bcb810cc6eaa6d0ddc4e83e9f8
Tom Bugnon <tombugnon@hotmail.com>
2021-07-08 13:18:26 -0500

    Update Main class (Parallel)

--- on_off_detection/__init__.py
+++ on_off_detection/__init__.py
@@ -14,6 +14,26 @@ DF_PARAMS = {
 }
 
 
+def run_cluster(
+	i,
+	cluster_id,
+	detection_func,
+	spike_times_list,
+	Tmax,
+	params,
+	output_dir,
+	debug_plot_filename,
+):
+	print(f'Run #{i+1}/N')
+	cluster_on_off_df = detection_func(
+		spike_times_list, Tmax, params,
+		save=True, output_dir=output_dir,
+		filename=f'{debug_plot_filename}_cluster={cluster_id}',
+	)
+	cluster_on_off_df['cluster_id'] = cluster_id
+	return cluster_on_off_df
+
+
 class OnOffModel(object):
 	"""Run ON and OFF-state detection from MUA data.
 	
@@ -30,7 +50,7 @@ class OnOffModel(object):
 	def __init__(
 		self, spike_times_list, cluster_ids=None, pooled_detection=True,
 		params=None, Tmax=None, method='threshold',
-		output_dir=None, debug_plot_filename=None,
+		output_dir=None, debug_plot_filename=None, hyp=None, n_jobs=50,
 	):
 		self.spike_times_list = [sorted(spike_times) for spike_times in spike_times_list]
 		if cluster_ids is not None:
@@ -50,6 +70,7 @@ class OnOffModel(object):
 		if params is None:
 			params = {}
 		self.params.update(params)
+		self.hyp = hyp
 		# Output stuff
 		self.output_dir = output_dir
 		if output_dir is not None:
@@ -68,17 +89,37 @@ class OnOffModel(object):
 			)
 		else:
 			print(f"Run on-off detection for each cluster (N={len(self.cluster_ids)})")
-			on_off_dfs = []
-			for i, cluster_id in tqdm(range(len(self.cluster_ids))):
-				print(f'Run #{i+1}/N')
-				cluster_on_off_df = self.detection_func(
-					[self.spike_times_list[i]], self.Tmax, self.params,
-					save=True, output_dir=self.output_dir/'plots',
-					filename=f'{self.debug_plot_filename}_cluster={cluster_id}',
+
+
+			if self.n_jobs == 1:
+				on_off_dfs = []
+				for i, cluster_id in tqdm(enumerate(range(len(self.cluster_ids)))):
+					print(f'Run #{i+1}/N')
+					cluster_on_off_df = self.detection_func(
+						[self.spike_times_list[i]], self.Tmax, self.params,
+						save=True, output_dir=self.output_dir/'plots',
+						filename=f'{self.debug_plot_filename}_cluster={cluster_id}',
+					)
+					cluster_on_off_df['cluster_id'] = cluster_id
+					on_off_dfs.append(cluster_on_off_df)
+			else:
+				from joblib import Parallel, delayed
+				on_off_dfs = Parallel(n_jobs=20, backend='multiprocessing')(
+					delayed(run_cluster)(
+						i,
+						cluster_id,
+						self.detection_func,
+						[self.spike_times_list[i]],
+						self.Tmax,
+						self.params,
+						self.output_dir/'plots',
+						self.debug_plot_filename
+					)
+					for i, cluster_id in enumerate(self.cluster_ids)
 				)
-				cluster_on_off_df['cluster_id'] = cluster_id
-				on_off_dfs.append(cluster_on_off_df)
+
 			self.on_off_df = pd.concat(on_off_dfs)
+
 		return self.on_off_df
 	
 	# def save():
@@ -87,6 +128,4 @@ class OnOffModel(object):
 	# 	self.output_dir.mkdir(exist_ok=True, parents=True)
 	# 	self.res.to_csv(self.output_dir/'on-off-times.csv')
 	# 	self.stats.to_csv(self.output_dir/'on-off-stats.csv')
-
-
 
